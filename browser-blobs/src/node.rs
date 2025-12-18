4657d694ad6356058a3fed4c09ba6f4863b47f6a
Volker Mische <volker.mische@gmail.com>
2025-12-19 00:58:52 +0100

    WIP: no it works
    It still needs a cleanup though

--- browser-blobs/src/node.rs
+++ browser-blobs/src/node.rs
@@ -5,7 +5,11 @@ use bao_tree::{Blake3Hasher, Hasher};
 use bytes::Bytes;
 use iroh::{discovery::static_provider::StaticProvider, protocol::Router, Endpoint, EndpointId};
 use iroh_blobs::{
-    api::{blobs::{BlobStatus, ImportMode, AddPathOptions}, downloader::Downloader, Store},
+    api::{
+        blobs::{AddPathOptions, BlobStatus, ImportMode},
+        downloader::Downloader,
+        Store,
+    },
     ticket::BlobTicket,
     BlobFormat, BlobsProtocol, Hash,
 };
@@ -14,13 +18,15 @@ use sha2::{Digest, Sha256};
 //type HasherToUse = Blake3Hasher;
 pub type HasherToUse = CommpHasher;
 
-
 // Based on Mistral:
 // write some rust code that takes 1024 bytes as input and calculate the root of its merkle tree. the hashing function should be sha2-256.
 fn calculate_merkle_root(data: &[u8]) -> Vec<u8> {
     // Split the data into 32-byte chunks
     let chunk_size = 32;
-    let mut hashes: Vec<Vec<u8>> = data.chunks(chunk_size).map(|chunk| chunk.to_vec()).collect();
+    let mut hashes: Vec<Vec<u8>> = data
+        .chunks(chunk_size)
+        .map(|chunk| chunk.to_vec())
+        .collect();
 
     // Build the Merkle tree
     while hashes.len() > 1 {
@@ -41,7 +47,6 @@ fn calculate_merkle_root(data: &[u8]) -> Vec<u8> {
     hashes[0].clone()
 }
 
-
 #[derive(Clone, Copy, Debug, Default, PartialEq)]
 pub struct CommpHasher;
 
@@ -51,13 +56,17 @@ impl Hasher for CommpHasher {
         // TODO vmx 2025-09-21: no clue when 20 and 64 bytes are hashed, so this is a hack for now
         // to at least keep things running.
         if data.len() < 64 {
-            return [0u8; 32].into()
+            return [0u8; 32].into();
         }
         let root = calculate_merkle_root(data);
         //println!("vmx: hash_chunk: root: {:X?}", root);
         <[u8; 32]>::try_from(&root[..32]).unwrap().into()
     }
-    fn hash_inner(left_child: &bao_tree::Hash, right_child: &bao_tree::Hash, _is_root: bool) -> bao_tree::Hash {
+    fn hash_inner(
+        left_child: &bao_tree::Hash,
+        right_child: &bao_tree::Hash,
+        _is_root: bool,
+    ) -> bao_tree::Hash {
         let data: Vec<_> = [&left_child.as_bytes()[..], &right_child.as_bytes()[..]].concat();
         let mut hashed = Sha256::digest(&data);
         //println!("vmx: inner data: {:X?}", data);
@@ -68,7 +77,6 @@ impl Hasher for CommpHasher {
         <[u8; 32]>::from(hashed).into()
     }
 }
-
 
 #[derive(Debug, Clone)]
 pub struct BlobsNode {
@@ -90,7 +98,10 @@ impl BlobsNode {
         let store = iroh_blobs::store::fs::FsStore::load::<HasherToUse>("datastore").await?;
         let downloader = Downloader::new::<HasherToUse>(&store, &endpoint);
         let router = Router::builder(endpoint)
-            .accept(iroh_blobs::ALPN, BlobsProtocol::<HasherToUse>::new(&store, None))
+            .accept(
+                iroh_blobs::ALPN,
+                BlobsProtocol::<HasherToUse>::new(&store, None),
+            )
             .spawn();
         Ok(Self {
             blobs: store.as_ref().clone(),
--- browser-blobs/src/wasm.rs
+++ browser-blobs/src/wasm.rs
@@ -1,17 +1,14 @@
 use anyhow::{anyhow, Result};
 use bao_tree::io::BaoContentItem;
 use bytes::Bytes;
-use iroh_blobs::{ticket::BlobTicket, Hash,
-    get::request::GetBlobItem,
-};
 use futures::channel::mpsc;
+use iroh_blobs::{get::request::GetBlobItem, ticket::BlobTicket, Hash};
 use js_sys::Uint8Array;
-use n0_future::StreamExt;
+use n0_future::{SinkExt, StreamExt};
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber_wasm::MakeConsoleWriter;
 use wasm_bindgen::{prelude::wasm_bindgen, JsError, JsValue};
 use wasm_streams::{readable::sys::ReadableStream as JsReadableStream, ReadableStream};
-
 
 #[wasm_bindgen(start)]
 fn start() {
@@ -49,39 +46,50 @@ impl BlobsNode {
         let ticket: BlobTicket = ticket.parse().map_err(to_js_err)?;
         //let hash = self.0.download(ticket).await.map_err(to_js_err)?;
         //Ok(hash.to_string())
-        let connection = self.0.endpoint().connect(ticket.addr().id, iroh_blobs::ALPN).await?;
-        let mut progress = iroh_blobs::get::request::get_blob::<crate::HasherToUse>(connection, ticket.hash());
+        let connection = self
+            .0
+            .endpoint()
+            .connect(ticket.addr().id, iroh_blobs::ALPN)
+            .await?;
+        let mut progress =
+            iroh_blobs::get::request::get_blob::<crate::HasherToUse>(connection, ticket.hash());
 
         let (mut tx, rx) = mpsc::channel::<Result<JsValue, JsValue>>(1);
 
-        loop {
-            match progress.next().await {
-                Some(GetBlobItem::Item(item)) => match item {
-                    BaoContentItem::Leaf(leaf) => {
-                        println!("vmx: data received: {:?}", &leaf.data);
-                        tracing::info!("vmx: data received: {:?}", &leaf.data);
-                        let js_value = Uint8Array::from(&leaf.data[..]).into();
-                        tx.try_send(Ok(js_value)).unwrap();
+        wasm_bindgen_futures::spawn_local(async move {
+            loop {
+                match progress.next().await {
+                    Some(GetBlobItem::Item(item)) => match item {
+                        BaoContentItem::Leaf(leaf) => {
+                            println!("vmx: data received: {:?}", &leaf.data);
+                            tracing::info!("vmx: data received: {:?}", &leaf.data);
+                            let js_value = Uint8Array::from(&leaf.data[..]).into();
+                            tx.send(Ok(js_value)).await.unwrap();
 
-                        //tokio::io::stdout().write_all(&leaf.data).await?;
+                            //tokio::io::stdout().write_all(&leaf.data).await?;
+                        }
+                        BaoContentItem::Parent(parent) => {
+                            tracing::info!("Parent: {parent:?}");
+                        }
+                    },
+                    Some(GetBlobItem::Done(stats)) => {
+                        //break stats;
+                        println!("vmx: stats: {:?}", stats);
+                        break;
                     }
-                    BaoContentItem::Parent(parent) => {
-                        tracing::info!("Parent: {parent:?}");
+                    Some(GetBlobItem::Error(err)) => {
+                        return Err(anyhow!("Error while streaming blob: {err}"))
+                            .map_err(to_js_err)
+                            .expect("vmx: error while streaming blob");
                     }
-                },
-                Some(GetBlobItem::Done(stats)) => {
-                    //break stats;
-                    println!("vmx: stats: {:?}", stats);
-                    break
-                }
-                Some(GetBlobItem::Error(err)) => {
-                    return Err(anyhow!("Error while streaming blob: {err}")).map_err(to_js_err);
-                }
-                None => {
-                    return Err(anyhow!("Stream ended unexpectedly.")).map_err(to_js_err);
+                    None => {
+                        return Err(anyhow!("Stream ended unexpectedly."))
+                            .map_err(to_js_err)
+                            .expect("vmx: error stream ended");
+                    }
                 }
             }
-        }
+        });
 
         let output_stream = ReadableStream::from_stream(rx).into_raw();
         Ok(output_stream)
